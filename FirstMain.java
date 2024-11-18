b980d1ab70761fa78fa9d40955444e4db65f9e2c
TalhaNALBANT <155415792+TalhaNALBANT@users.noreply.github.com>
2024-11-18 19:44:44 +0300

    Update FirstMain.java

--- FirstMain.java
+++ FirstMain.java
@@ -1,5 +1,7 @@
 ﻿public class FirstMain {
     public static void main(String[] args) {
-        System.out.println("We are learning git together, jubilation!!");
+        System.out.println("We are learning git together , jubilation!!");
+        System.out.println("Everything is teamWork");
+        
     }
 }
