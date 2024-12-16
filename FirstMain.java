92644313db89fa992e5ffc250cad2ea600b8dc9e
EfeDurmaz16 <75971010+EfeDurmaz16@users.noreply.github.com>
2024-12-16 11:34:03 +0300

    Delete FirstMain.java

--- FirstMain.java
+++
@@ -1,7 +0,0 @@
-﻿public class FirstMain {
-    public static void main(String[] args) {
-        System.out.println("We are learning git together , jubilation!!");
-        System.out.println("Everything is teamWork");
-        
-    }
-}
